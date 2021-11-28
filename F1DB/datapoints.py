1e3063f80055bbf12326cca51254b9f2bfa4001f
OZ2308 <ossama994@gmail.com>
2021-11-28 22:07:52 +0000

    Updated & added classes
    Updated and added classes in datapoint and tidyed up db_script

--- F1DB/datapoints.py
+++ F1DB/datapoints.py
@@ -1,49 +1,49 @@
 import requests
 from bs4 import BeautifulSoup
 
-class constructors():
-    year = 1986
-    base_url = f"http://ergast.com/api/f1/{year}"
+class constructors:
+	def __init__(self, year) -> None:
+		self.year = year
+		self.base_url = f"http://ergast.com/api/f1/{self.year}"
 
-    def constructors_func(self):
-        constructors = f"{self.base_url}/constructors"
-        site = requests.get(constructors)
-        siteSRC = site.content
-        # print(siteSRC)
+	def constructors_func(self):
+		constructors = f"{self.base_url}/constructors"
+		site = requests.get(constructors)
+		siteSRC = site.content
 
-        soup = BeautifulSoup(siteSRC, 'html.parser')
-        teams = soup.find_all('name')
+		soup = BeautifulSoup(siteSRC, 'html.parser')
+		teams = soup.find_all('name')
 
-        ## BULK GET CONSTRUCTORS ##
-        val = []
-        for team in teams:
-            val.append(tuple(team))
-        return val
+		## BULK GET CONSTRUCTORS ##
+		val = []
+		for team in teams:
+			val.append(tuple(team))
+		return val
 
 class drivers:
-    year = 1986
-    base_url = f"http://ergast.com/api/f1/{year}"
+	year = 1986
+	base_url = f"http://ergast.com/api/f1/{year}"
 
-    def driver_names(self):
-        drivers = f"{self.base_url}/drivers"
-        site = requests.get(drivers)
-        siteSRC = site.content
-        soup = BeautifulSoup(siteSRC, 'html.parser')
+	def driver_names(self):
+		drivers = f"{self.base_url}/drivers"
+		site = requests.get(drivers)
+		siteSRC = site.content
+		soup = BeautifulSoup(siteSRC, 'html.parser')
 
-        given_name  = (soup.find_all('givenname'))
-        family_name = (soup.find_all('familyname'))
+		given_name  = (soup.find_all('givenname'))
+		family_name = (soup.find_all('familyname'))
 
-        ## BULK GET DRIVERS ##
-        firstname = []
-        for name in given_name:
-            firstname.append(name.text)
+		## BULK GET DRIVERS ##
+		firstname = []
+		for name in given_name:
+			firstname.append(name.text)
 
-        surname = []
-        for name in family_name:
-            surname.append(name.text)
+		surname = []
+		for name in family_name:
+			surname.append(name.text)
 
-        formatnames = zip(firstname, surname)
-        vals = list(formatnames)
+		formatnames = zip(firstname, surname)
+		vals = list(formatnames)
 
 ## TESTING YEARS INPUT ##
 
--- F1DB/db_script.py
+++ F1DB/db_script.py
@@ -1,13 +1,15 @@
+from typing import Optional
 import requests
 from bs4 import BeautifulSoup
 import mysql.connector
+from datapoints import constructors
 
 ## CREATE & CONNECT TO DB ##
 mydb = mysql.connector.connect(
-  host="127.0.0.1",
-  user="root",
-  password="f1root",
-  database="mydatabase"
+    host="127.0.0.1", 
+    user="root", 
+    password="f1root", 
+    database="mydatabase"
 )
 
 mycursor = mydb.cursor()
@@ -35,21 +37,30 @@ mycursor.execute("CREATE TABLE IF NOT EXISTS drivers (id INT NOT NULL AUTO_INCRE
 
 ## INSERT DATA INTO TABLES ##
 teamquery = "INSERT INTO constructors (name) VALUES (%s)"
-mycursor.executemany(teamquery,val)
+mycursor.executemany(teamquery,)
 mydb.commit()
 
-namequery = "INSERT INTO drivers (firstname, surname) VALUES (%s, %s)"
-mycursor.executemany(namequery,vals)
-mydb.commit()
+# namequery = "INSERT INTO drivers (firstname, surname) VALUES (%s, %s)"
+# mycursor.executemany(namequery,vals)
+# mydb.commit()
 
-# SHOW ALL ROWS IN TABLE ##
-mycursor.execute("SELECT * FROM drivers")
-myresult = mycursor.fetchall()
- 
-for result in myresult:
-    print(result)
+## SHOW ALL ROWS IN TABLE ##
+def table_results(table: str, limit: Optional[int] = None):
+    sql = f"SELECT * FROM {table}"
+    sql_with_limit = f"SELECT * FROM {table} LIMIT {limit}"
 
+    mycursor.execute(sql_with_limit if limit else sql)
+    myresult = mycursor.fetchall()
 
-## DELETE TABLE ##
-# del_table = "DROP TABLE *******table name*******"
-# mycursor.execute(del_table)
+    for result in myresult:
+        print(result)
+
+# table_results()
+
+#TODO FIND IF CAN CALL FUNC FROM TERMINAL OR CREATE FILE
+
+# def delete_table(table):
+#     del_table = f"DROP TABLE {table}"
+#     mycursor.execute(del_table)
+
+# delete_table('constructors')
